049d8f913e598d8d9357bf30dffa98db1cdb9d44
Ombre <krys1014@wp.pl>
2022-07-15 18:23:01 +0200

    Change structure and add footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,32 @@ import './App.scss';
 import Form from './components/Form';
 
 function App() {
+  const footerTxt = '© Stworzone przez Krystian Heluszka dla Urząd Miejski w Wołowie';
+
   return (
-    <div className="App">
+    <>
       <header className="App-header">
-        <h1 className='title'>Generator kodu stopki mailingowej</h1>
-        <Form />
+        <div className="left-side">
+          <img className='image-herb' src="/assets/images/herb.png" alt="Herb" />
+        </div>
+        <div className="center">
+          <h1 className='title'>Generator kodu stopki mailingowej</h1>
+        </div>
+        <div className="right-side">
+          <img className='image-herb' src="/assets/images/herb.png" alt="Herb" />
+        </div>
       </header>
-    </div>
+
+      <section>
+        <Form />
+      </section>
+
+      <footer>
+        <div className="footer">
+          {footerTxt}
+        </div>
+      </footer>
+    </>
   );
 }
 
