133cc48379f6440528aebceb1e1246fd80d6a548
Ombre <krys1014@wp.pl>
2022-07-24 17:26:00 +0200

    Change text footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,22 @@
 import React from 'react'
 import './App.scss';
 import Form from './components/Form';
+import img from './assets/image/herb.png';
 
 function App() {
-  const footerTxt = '© Stworzone przez Krystian Heluszka dla Urząd Miejski w Wołowie';
+  const footerTxt = 'Cypyright © 2022 by Krystian Heluszka. Wszelkie prawa zastrzeżone.';
 
   return (
     <>
       <header>
         <div className="left-side">
-          <img className='image-herb' src="/assets/images/herb.png" alt="Herb" />
+          <img className='image-herb' src={img} alt="Herb" />
         </div>
         <div className="center">
           <h1 className='title'>Generator kodu stopki</h1>
         </div>
         <div className="right-side">
-          <img className='image-herb' src="/assets/images/herb.png" alt="Herb" />
+          <img className='image-herb' src={img} alt="Herb" />
         </div>
       </header>
 
